d781b495459f018d71ebb5ffc4c042ee49cad75c
Faisal Wicahyono <110772823+Faisalwich@users.noreply.github.com>
2023-07-14 16:39:22 +0700

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,22 @@
+// toggle class active
+
+const navigasi = document.querySelector(".navigasi");
+
+//ketika hamburger menu di klik
+document.querySelector;
+{
+  ("#hamburger-menu");
+}
+onclick = () => {
+  navigasi.classList.toggle("active");
+};
+
+//Klik di luar sidebar untuk menghilangkan nav
+/*
+const hamburger = document.querySelector("#hamburger-menu");
+
+document.addEventListener("click", function (e) {
+  if (!hamburger.contains(e.target) && !navigasi.contains(e.target)) {
+    navigasi.classList.remove("active");
+  }
+});*/
